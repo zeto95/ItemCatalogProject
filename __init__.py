543233878413a9af0e700ef0bd33e03f0190cd10
zeto95 <maielzayat95@gmail.com>
2018-04-29 17:45:59 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -66,7 +66,7 @@ def gconnect():
     url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
            % access_token)
     h = httplib2.Http()
-    result = json.loads(h.request(url, 'GET')[1])
+    result = json.loads(h.request(url, 'GET')[1].decode('utf-8'))
     # If there was an error in the access token info, abort.
     if result.get('error') is not None:
         response = make_response(json.dumps(result.get('error')), 500)
